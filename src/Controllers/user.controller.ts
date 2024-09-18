f0f24cbf05565d75b9417005d9bde421ff91f48d
Wellington Nobre <Wellington.quaresma@oi.net.br>
2024-09-18 09:49:23 -0300

    feature/ adicionando rotas para buscar usuarios

--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -1,14 +1,43 @@
-import { Controller, Post , Body } from '@nestjs/common'
+import { Controller, Post , Body, Param, BadRequestException, Get, UseGuards } from '@nestjs/common'
 import { UserService } from '../Services/user.service'
 import { CreateUserDto } from 'src/Dtos/create-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller ('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    //rota para criar usuario
     @Post('register')
+    @ApiOperation({ summary: 'Cria novo usuario'})
     async register(@Body() createUserDto: CreateUserDto) {
         const { email, password } = createUserDto
         return this.userService.createUser(createUserDto);
     }
+
+    // rota para buscar usuario pelo username
+    @Get('username/:username')
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Busca usuarios pelo username'})
+    async getUserByUsername(@Param('username') username: string) {
+        const user = await this.userService.findByUsername(username);
+        if(!user) {
+            throw new BadRequestException(`Usuário com username ${username} não encontrado.`); 
+        }
+        return user;
+    }
+
+    // rota para buscar usuario pelo email
+    @Get('email/:email')
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Busca usuario pelo email'})
+    async getUserByEmail(@Param('email')email: string) {
+        const user = await this.userService.findByEmail(email);
+        if(!user) {
+            throw new BadRequestException(`Usuário com E-mail ${email} não encontrado.`)
+        }
+        return user
+    }
+
 }
